76f2e7bfa45012e231ab37bc862399127b58a929
Valentin Bud <valentin@databus.pro>
2014-04-07 15:25:12 +0300

    Added the first version of debian-unattended.sh and the vanilla preseed.

--- debian-unattended.sh
+++ debian-unattended.sh
@@ -1,10 +1,43 @@
+#!/bin/bash
+
+# Install Prerequisites
+sudo apt-get install virtinst -y
+
+# Apply vanilla preseed config if $1
+# TO DO: Check for file existence
+FLAVOR=$1
+
+if test -n "$FLAVOR" ; then 
+    CONFIG=$FLAVOR
+else
+    CONFIG=vanilla
+fi
+
+cp preseed/$CONFIG /tmp/preseed.cfg
+
+# Create QCOW2 Image
+# TO DO: Image size
+IMAGE=$CONFIG-debian-stable.qcow2
+qemu-img create -f qcow2 $IMAGE 100G
+
+# Get pwd
+PWD=`pwd`
+
+# virsh start network default
+sudo virsh net-start default
+
+# virt-install Debian
 sudo virt-install --connect qemu:///system \
---location=http://ftp.ro.debian.org/debian/dists/stable/main/installer-amd64 \
---initrd-inject=/home/bud/code/debian-unattended/preseed.cfg \
+--location=http://http.debian.net/debian/dists/stable/main/installer-amd64 \
+--initrd-inject=/tmp/preseed.cfg \
 --extra-args="auto console=tty0 console=ttyS0,115200 DEBIAN_FRONTEND=text" \
---name d-i --ram=512 \
---disk=path=/home/bud/code/debian-unattended/debian-stable.qcow2,bus=virtio,driver_name=qemu,driver_type=qcow2 \
+--name d-i --ram=1024 \
+--disk=path=$PWD/$IMAGE,bus=virtio,driver_name=qemu,driver_type=qcow2 \
 --graphics none \
---serial file,path=/tmp/debian-stable-preseed \
+--serial file,path=/tmp/debian-unattended \
 --noreboot \
+--wait 10 \
 --network=network=default,model=virtio
+
+# Remove leftovers
+rm /tmp/preseed.cfg /tmp/debian-unattended
